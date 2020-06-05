3d5981159d6ef3b140603579a3e930c2f5a53f74
Mikuno <35458500+Mikuno@users.noreply.github.com>
2020-06-05 23:28:59 +0200

    Update EasyFile.bat

--- EasyFile.bat
+++ EasyFile.bat
@@ -6,6 +6,6 @@ SET location=
 SET /p name= Name of the file : 
 SET /p extension= Extension of the file ( without "." ) : 
 SET /p location= Destination of the file : 
-cd %chemi% 
+cd %location% 
 copy nul %name%.%extension%
 pause
